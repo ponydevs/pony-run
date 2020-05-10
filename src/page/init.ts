b1f995d4f711e7404d5503d5ae6ab370b5723cf7
Mathieu CAROFF <mathieu.caroff@free.fr>
2020-05-10 18:38:11 +0200

    page (link add): github corner repository link

--- src/page/init.ts
+++ src/page/init.ts
@@ -1,17 +1,20 @@
 import { h } from './lib/hyper';
+import { githubCornerHTML } from './lib/githubCorner';
 
 export const init = () => {
     const canvas = h('canvas');
     canvas.width = 800;
     canvas.height = 600;
 
-    document.body.appendChild(
+    document.body.append(
         h('h1', {
             textContent: 'Pony Run',
         }),
+        canvas,
+        h('div', {
+            innerHTML: githubCornerHTML('https://github.com/ponydevs/pony-run'),
+        }),
     );
-
-    document.body.appendChild(canvas);
 
     return { canvas };
 };
